f2a888cfc40af5eba2e4eef4038f65226ffd6a8f
devongillis <devon.gillis@ucalgary.ca>
2019-10-12 20:48:40 -0600

    add new project

---
+++ Unity_3D_Third_Person_Unity_Game/Assets/character_blink.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character_blink : MonoBehaviour
+{
+	private Animator anim;
+	public int upper = 10;
+	// Start is called before the first frame update
+	void Start()
+	{
+		anim = gameObject.GetComponentInChildren<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//int digit = Random.Range(0, 10);
+		//Debug.Log(digit);
+		if(Random.Range(0, upper) == 0){
+			anim.SetTrigger("blink");
+		}
+	}
+}
---
+++ Unity_3D_Third_Person_Unity_Game/Assets/character_script_controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character_script_controller : MonoBehaviour
+{
+
+	private Animator anim;
+	private float speed = 0.0f;
+	public float interval = 0.001f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		anim = gameObject.GetComponentInChildren<Animator>();
+		anim.SetFloat("speed", speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//anim.speed += 0.01f;
+		speed += interval;
+		anim.SetFloat("speed", speed);
+    }
+}
