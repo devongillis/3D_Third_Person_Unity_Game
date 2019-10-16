3959d88208071fdaf21eb0397f261faad5ad7818
devongillis <devon.gillis@ucalgary.ca>
2019-10-16 02:02:31 -0600

    small changes to code and triggers
    camera now has different settings to use for various world objectives, inside the maze the camera pitch is limited to introduce a more aerial view of the maze

---
+++ Unity_3D_Third_Person_Unity_Game/Assets/Scripts/camera scripts/onTriggerEnterForCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onTriggerEnterForCamera : MonoBehaviour
+{
+	public GameObject mainCamera;
+	cameraController script;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		script = mainCamera.GetComponent<cameraController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void OnTriggerEnter(Collider other){
+		if(other.tag == "MainCamera"){
+			script.useRegularPitch = false;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if(other.tag == "MainCamera"){
+			script.useRegularPitch = true;
+		}
+	}
+}
